5a81198fd2946782e13365b590e8666bfcc98025
Nnamdi <nahorr@gmail.com>
2018-12-17 19:51:47 -0700

    resolve paystach chrome mime issue

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -9,7 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Nahorr Analytics') }}</title>
 
-
+    <base hrefs="/">
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
